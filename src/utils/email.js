7063157e08155446e3d92809c26e6efdaaf2648c
vcin-bit <risksecured.consultancy@gmail.com>
2026-03-08 23:26:00 +0000

    Update email.js

--- src/utils/email.js
+++ src/utils/email.js
@@ -2,7 +2,6 @@ const sgMail = require('@sendgrid/mail');
 
 if (process.env.SENDGRID_API_KEY) {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  sgMail.client.setDefaultRequest('baseUrl', 'https://api.eu.sendgrid.com');
 }
 
 const FROM = process.env.SENDGRID_FROM || 'onboarding@doblive.co.uk';
