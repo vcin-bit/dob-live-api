cd9832ada2a2f8cf0004a7d3b0876dff2f4be981
vcin-bit <risksecured.consultancy@gmail.com>
2026-03-14 12:53:24 +0000

    Create PatrolChecklist.js

---
+++ src/models/PatrolChecklist.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router  = express.Router();
+const { authenticate, requireRole } = require('../middleware/auth');
+const { getChecklist, saveChecklist, flagCheckpoint } = require('../controllers/patrolChecklistController');
+
+router.get('/',      authenticate, getChecklist);
+router.put('/',      authenticate, requireRole('COMPANY', 'SUPER_ADMIN'), saveChecklist);
+router.post('/flag', authenticate, flagCheckpoint);
+
+module.exports = router;
